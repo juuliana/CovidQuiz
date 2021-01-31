6ff83a783e94aa887eeb9dd5c327d077a9114fd8
juuliana <jumaju10@hotmail.com>
2021-01-30 22:29:09 -0300

    Footer --update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,10 +6,17 @@ const FooterWrapper = styled.footer`
   display: flex;
   align-items: center;
   border-radius: 4px;
+
   img {
-    width: 130px;
-    margin-right: 23px;
+    width: 70px;
+    margin-left: 40px;
   }
+
+  img.saude{
+    width: 200px;
+    margin-left: 70px;
+  }
+
   a {
     color: white;
     text-decoration: none;
@@ -28,11 +35,11 @@ export default function Footer(props) {
   return (
     // eslint-disable-next-line react/jsx-props-no-spreading
     <FooterWrapper {...props}>
-      <a href="https://www.alura.com.br/">
+      <a href="https://www.alura.com.br/" target="_blank">
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
-      <a href="https://coronavirus.saude.gov.br/">
-        <img src="https://portalarquivos.saude.gov.br/images/png/2020/April/14/logo.png" alt="Logo Ministerio" />
+      <a href="https://coronavirus.saude.gov.br/" target="_blank">
+        <img class="saude" src="https://portalarquivos.saude.gov.br/images/png/2020/April/14/logo.png" alt="Logo Ministerio" />
       </a>
     </FooterWrapper>
   );
